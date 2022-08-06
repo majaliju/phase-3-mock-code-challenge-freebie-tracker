351bfd564596985509b36ba98a841acf74ee4394
majaliju <majalijunyc@gmail.com>
2022-08-05 20:16:44 -0400

    updated -- via freebie tracker lab walkthru, had help on this one

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -2,4 +2,8 @@ class Freebie < ActiveRecord::Base
   belongs_to :company
   belongs_to :dev
 
+  def print_details
+    binding.pry
+   "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+  end
 end
